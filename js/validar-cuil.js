cc4ce810f08584d9aac9a375984b62a004bdc142
unknown <abdielmamani@upeu.edu.pe>
2024-06-03 19:22:06 -0500

    validar js

--- js/validar-cuil.js
+++ js/validar-cuil.js
@@ -1,8 +1,16 @@
 export default function esUnCuil(campo){
     const cuil = campo.value.replace(/[-\/]/g, "");
-    numerosRepetidos(cuil)
-    console.log(cuil);
-    console.log(numerosRepetidos(cuil));
+    
+    if(numerosRepetidos(cuil)){
+        console.log("Valores repetidos");
+    }else{
+        if(validarPrimerosDigitos(cuil) && validarDigVerificador(cuil)){
+            console.log("cuil válido");
+        }else{
+            console.log("cuil no válido");
+        }
+    }
+   
 }
 function numerosRepetidos(cuil){
     const numeroRep = [
@@ -13,4 +21,26 @@ function numerosRepetidos(cuil){
         "44444444444",
     ];
     return numeroRep.includes(cuil);
+}
+function validarPrimerosDigitos(cuil){
+    let primerosDigitos = cuil.substr(0,2);
+    let digitosValido = ['20','23','24','25'];
+   return digitosValido.includes(primerosDigitos)
+
+}
+function validarDigVerificador(cuil){
+    let acumulado = 0 ; 
+    const factores=[5,4,3,2,7,6,5,4,3,2]
+    for(let i=0;i<10;i++){
+        acumulado+= parseInt(cuil[i],10) * factores[i];
+    }
+    let validarTeorico=11 - (acumulado % 11);
+    if(validarTeorico==11){
+        validarTeorico=0;
+    }else if(validarTeorico ==10){
+        validarTeorico=9
+    }
+    const digitoVerificador=parseInt(cuil[10],10)
+    console.log(digitoVerificador);
+    return digitoVerificador === validarTeorico;
 }
